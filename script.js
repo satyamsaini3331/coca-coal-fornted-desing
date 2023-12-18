a1d9bcc4e2ea1d6394ecca9be4b5c82756eb6fb9
satyamsaini3331 <154007732+satyamsaini3331@users.noreply.github.com>
2023-12-18 08:53:14 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+gsap.from("h2",{
+    scale:1.2,
+    duration:1.1,
+    delay:1.1,
+    opacity: 0,
+});
+gsap.from("img",{
+    scale: 1.1,
+    duration:1.0,
+    delay:4.2,
+    opacity:0,
+})
+gsap.from("#center-bg",{
+    scale: 1.3,
+    duration:3.0,
+    delay: 2.1,
+    opacity:0,
+})
